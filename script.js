d62298c02ba71da2adf6608aca7aa0cd24a825bf
Rushikesh Chaudhari <rushichaudhari1237@gmail.com>
2021-01-19 02:20:29 +0530

    setup-basic-template

---
+++ script.js
@@ -0,0 +1,39 @@
+  
+var aText = new Array(
+    "Hello buddy,", // add your text
+    "Greetings from team Aavishkar'21", // add your text
+    "We are working on our website", // add your text
+    "Will publish it soon, Stay tuned", // add your text
+    "For more updates follow @aavishkar_cek",
+    "on Instagram." // add your text
+    );
+    var iSpeed = 100; 
+    var iIndex = 0; 
+    var iArrLength = aText[0].length; 
+    var iScrollAt = 20; 
+     
+    var iTextPos = 0; 
+    var sContents = ''; 
+    var iRow; 
+     
+    function typewriter()
+    {
+     sContents =  ' ';
+     iRow = Math.max(0, iIndex-iScrollAt);
+     var destination = document.getElementById("typedtext");
+     
+     while ( iRow < iIndex ) {
+      sContents += aText[iRow++] + '<br />';
+     }
+     destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+     if ( iTextPos++ == iArrLength ) {
+      iTextPos = 0;
+      iIndex++;
+      if ( iIndex != aText.length ) {
+       iArrLength = aText[iIndex].length;
+       setTimeout("typewriter()", 500);
+      }
+     } else {
+      setTimeout("typewriter()", iSpeed);
+     }
+    }
